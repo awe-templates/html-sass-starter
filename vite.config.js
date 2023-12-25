57d620bb5ca94a17ef71e2345b6acceaf004d3b3
relliv <epsilonelittr@gmail.com>
2023-12-26 01:09:26 +0300

    Add example pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import Checker from "vite-plugin-checker";
 
+const { resolve } = require('path')
+
 export default defineConfig({
   // Plugins
-  plugins: [Checker({ typescript: true })],
+  plugins: [
+    Checker({ typescript: true }),
+  ],
 
   // Base public path (default is '/')
   base: "/",
@@ -27,4 +31,11 @@ export default defineConfig({
       plugins: [tailwindcss],
     },
   },
+
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'index.html'),
+      nested: resolve(__dirname, '/src/pages/home.html')
+    }
+  }
 });
